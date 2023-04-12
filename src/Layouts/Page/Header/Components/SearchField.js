5e97d71cdd0b1fd88ccc2963eb6744d2a9ab2207
lisabarn <130385193+lisabarn@users.noreply.github.com>
2023-04-12 13:45:04 +0000

    SecondaryButton

--- src/Layouts/Page/Header/Components/SearchField.js
+++ src/Layouts/Page/Header/Components/SearchField.js
@@ -15,7 +15,7 @@ const Input = styled.input`
     color: #94A3B8;
     width: 100%;
     &:hover {
-        background: linear-gradient(180deg, #DFEAF1 0%, #CFDDE8 100%);
+        background: rgba(248,250,252 0.8);
     }
     &:focus {
         background: white;
@@ -23,7 +23,6 @@ const Input = styled.input`
 `;
 
 const SearchIconWrapper = styled.div`
-    background-color: #4B759E;
     position: absolute;
     box-sizing:border-box;
     right: 0px;  /* Здесь можно добавить отступ от правого края, чтобы сдвинуть иконку левее */
@@ -34,7 +33,7 @@ const SearchIconWrapper = styled.div`
 function SearchField() {
     return (
         <WrapperInput className='d-inline-flex'>  {/* Устанавливает ширину поля и включает относительное положение */}
-            <Input className='p-2' placeholder='Найти на странице...'/>
+            <Input className='p-2' placeholder='Найти'/>
             <SearchIconWrapper className='p-2'>  {/* Устанавливает абсолютное положение внутри поля ввода */}
                 <IconOnlyButton icon={<SearchIcon/>}/>
             </SearchIconWrapper>
